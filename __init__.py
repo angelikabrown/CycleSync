10430bde725b8a6a4b60ffab131a5e422ac0f6f6
Angelika Brown <angelikpbrown@gmail.com>
2025-04-21 16:15:31 -0400

    connected to sqlite

--- __init__.py
+++ __init__.py
@@ -6,7 +6,7 @@ from routes import rt
 def create_app():
     app = Flask(__name__)  
     # app.config['SECRET_KEY'] = 'your_secret_key'
-    # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+    # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cyclesync.db'
     # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     # app.config['FLASK_ENV'] = 'development'
     # app.config['DEBUG'] = True
@@ -16,6 +16,7 @@ def create_app():
     app.add_url_rule('/', endpoint='index')
 
     from main import db
+   
     db.init_app(app)
 
     with app.app_context():
